ceb5db4134c88b51cfcba239bd5c3ce41ce6a8e8
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-04-11 02:59:48 +0530

    Loading 2 Screens Done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.scanshield_mobile_app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.scanshield_mobile_app"
--- app/src/main/java/com/example/scanshield_mobile_app/loading2.java
+++ app/src/main/java/com/example/scanshield_mobile_app/loading2.java
@@ -3,7 +3,6 @@ package com.example.scanshield_mobile_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,25 +11,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class loading2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_loading2);
 
-        //For testing - logo click
-        ImageView logo = findViewById(R.id.imageView);
-        logo.setOnClickListener(new View.OnClickListener() {
+        //Testing- Logo2 Click
+        ImageView logo2 = findViewById(R.id.imageView2);
+        logo2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent logoclick = new Intent(getApplicationContext(), loading2.class);
-                startActivity(logoclick);
+                Intent logo2Click =  new Intent(getApplicationContext(), get_started_1.class);
+                startActivity(logo2Click);
             }
         });
-
-
 
 
 
