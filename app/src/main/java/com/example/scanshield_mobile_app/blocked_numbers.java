d01aa1efd9f8ff0e5d40f356e00e4a3e332eba16
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-04-27 18:02:03 +0530

    user check

--- app/src/main/java/com/example/scanshield_mobile_app/blocked_numbers.java
+++ app/src/main/java/com/example/scanshield_mobile_app/blocked_numbers.java
@@ -1,14 +1,26 @@
 package com.example.scanshield_mobile_app;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;import android.Manifest;
+
+
 public class blocked_numbers extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    TextView lUser;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +28,23 @@ public class blocked_numbers extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_blocked_numbers);
 
+        userCheck();
+
     }
+
+    public void userCheck(){
+        mAuth = FirebaseAuth.getInstance();
+        lUser = findViewById(R.id.LogUserEmailHome);
+        user = mAuth.getCurrentUser();
+
+        if (user == null){
+
+        }
+        else {
+            lUser.setText(user.getEmail());
+        }
+    }
+
+
+
 }
