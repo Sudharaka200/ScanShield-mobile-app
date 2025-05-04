5883bed3ad0ccbd78894377a53f8bfef2de126f0
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-05-04 22:04:44 +0530

    Call F(Gettters and setters)

--- app/src/main/java/com/example/scanshield_mobile_app/call_F.java
+++ app/src/main/java/com/example/scanshield_mobile_app/call_F.java
@@ -1,4 +1,47 @@
 package com.example.scanshield_mobile_app;
 
 public class call_F {
+    private String email;
+    private String phonenumber;
+    private String status;
+    private String datetime;
+
+    public call_F(String email, String phonenumber, String status, String datetime) {
+        this.email = email;
+        this.phonenumber = phonenumber;
+        this.status = status;
+        this.datetime = datetime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
 }
